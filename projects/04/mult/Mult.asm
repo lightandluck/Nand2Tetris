1aca434275d29bc05ec3e90bd1a9df570543c3e8
Kevin Nguyen <kevin.nguyen@lineophile.com>
2016-06-06 16:36:09 -0700

    Finished and tested mult.asm

--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -7,3 +7,49 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+
+@sum //set up sum variable
+M=0
+
+@R0 //check if any inputs are 0, jump directly to end
+D=M
+@STOP
+D;JEQ
+
+@R1 //check for 0, jump to END
+D=M
+@STOP
+D;JEQ
+
+@i //set iterator
+M=1
+
+(LOOP)
+    @i //loop condition
+    D=M
+    @R0
+    D=D-M
+    @STOP
+    D;JGT
+    
+    @R1 //add sum
+    D=M
+    @sum
+    M=M+D
+    
+    @i //increase iterator
+    M=M+1
+    
+    @LOOP
+    0;JMP
+
+(STOP)
+    @sum //set R2 to sum
+    D=M
+    @R2
+    M=D
+(END)  
+    @END
+    0;JMP
+    
+     
