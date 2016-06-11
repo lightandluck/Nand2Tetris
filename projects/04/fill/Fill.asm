01bb36bcd20165a772b1811678180eca5de6d97b
Kevin Nguyen <kevin.nguyen@lineophile.com>
2016-06-10 18:09:41 -0700

    Completed Fill.asm assignment

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -9,3 +9,70 @@
 // program clears the screen, i.e. writes "white" in every pixel.
 
 // Put your code here.
+
+(LOOP)
+//setting our variables screen, i (iterator), 
+//n (number of registers to loop over = total registers for screen memory map)
+@SCREEN 
+D=A
+@addr
+M=D
+
+@8192
+D=A
+@n
+M=D
+
+@i
+M=1
+
+@KBD
+D=M
+
+@BLACK
+D;JGT
+
+@WHITE
+D;JEQ
+
+//Fill screen with black loop
+(BLACK)
+    @i //not condition
+    D=M
+    @n
+    D=D-M
+    @LOOP
+    D;JGT
+    
+    @addr 
+    A=M
+    M=-1 //set register to all 1's (-1 in 2's complement)
+    
+    @addr //iterate addr and i
+    M=M+1
+    @i
+    M=M+1
+    
+    @BLACK
+    0;JMP   
+
+//Clear screen. Same logic as black, but sets registers to 0 instead
+(WHITE)
+    @i
+    D=M
+    @n
+    D=D-M
+    @LOOP
+    D;JGT
+    
+    @addr
+    A=M
+    M=0
+    
+    @addr
+    M=M+1
+    @i
+    M=M+1
+    
+    @WHITE
+    0;JMP   
