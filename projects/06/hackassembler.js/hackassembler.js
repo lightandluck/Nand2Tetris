e38ff8eb2c53cbcf0f639f52fba051be13e06a6e
Kevin Nguyen <kevin.nguyen@lineophile.com>
2016-06-27 12:18:45 -0500

    Able to write file without comments

--- projects/06/hackassembler.js/hackassembler.js
+++ projects/06/hackassembler.js/hackassembler.js
@@ -1,13 +1,14 @@
 'use strict';
 
 //grabs file name from arguments
-var fileName = process.argv[2];
+var readfileName = process.argv[2];
+var writeFileName = "test_output.txt";
 
 const readline = require('readline');
 const fs = require('fs');
 
 const rl = readline.createInterface({
-    input: fs.createReadStream(fileName)
+    input: fs.createReadStream(readfileName)
 })
 
 var lineNumber = 0;
@@ -18,9 +19,14 @@ rl.on('line', (line) => {
     var re = /(\/\/.*)|(^\s*$)/gm;
     
     if(!re.exec(line)) {
-        //console.log('command:' + lineNumber++, line);
+        strNew += line.toString() + '\n';
     }
-})
+});
+
+rl.on('close', function() {
+    fs.writeFile(writeFileName, strNew.trim(), (err) => {
+        if (err) throw err;
+    });
+});
 
 
-rl.on('close', () => console.log ('done'));
