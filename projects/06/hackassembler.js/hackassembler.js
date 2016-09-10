f6d2f42675c5b4fe56fca0d6f5a72e68746c5717
Kevin Nguyen <kevin.nguyen@lineophile.com>
2016-09-10 15:16:46 -0700

    Moved second pass to function.

--- projects/06/hackassembler.js/hackassembler.js
+++ projects/06/hackassembler.js/hackassembler.js
@@ -13,20 +13,12 @@ var Readable = require('stream').Readable;
 var readFileName = process.argv[2];
 var writeFileName = utility.getWriteFileName(readFileName);
 
-
-var lineNumber = 0; //Counter to keep track of what line number we are on, used so that labels specify correct line
-var symbolNumber = 16;  //First location to assign as address for variables
-var strNoLabels = '';   //For first pass to remove labels
-var strNoSymbols = '';    //For second pass to replace symbols
-var strHack = '';       //For last pass to convert to binary
-
 const rl = readline.createInterface({
     input: fs.createReadStream(readFileName)
 });
 
-
 var firstPass = runFirstPass();
-
+var secondPass = runSecondPass();
 //First pass to assign values to Labels and remove them.
 rl.on('line', (line) => {
     firstPass.parseLine(line);
@@ -35,47 +27,21 @@ rl.on('line', (line) => {
 rl.on('close', function() {
     
     const rl2 = readline.createInterface({
-        input: getReadable(firstPass.getString())
+        input: getReadable(firstPass.getString().trim())
     })
 
     //Second pass - to replace symbols with numbers
-    rl2.on('line', (line) => {        
-        var newline = '';
-
-        var commandType = Parser.commandType(line);    
-        if (commandType === 'A_COMMAND') {
-            var aSymbol = Parser.parseACommand(line);
-            //check if A-command is a symbol or number
-
-            if (isNaN(aSymbol)) {
-                //We're looking at a symbol like @R0,
-                //Check if symbol already exists, usually for predefined symbols
-                if (SymbolTable.contains(aSymbol)) {
-                    newline = '@' + SymbolTable.getAddress(aSymbol);                
-                } else { //change add to symbol table new variables
-                    newline = '@' + symbolNumber;
-                    SymbolTable.addEntry (aSymbol, symbolNumber++);
-                }
-            } else {
-                newline = line;
-            }
-        } else {
-            newline = line;
-        }
-        strNoSymbols += newline + '\n';        
+    rl2.on('line', (line) => {  
+        secondPass.parseLine(line);      
+               
     });
 
-
     rl2.on ('close', function() {
-        //creates stream to pass to next pass
-        var streamNoSymbols = new Readable;
-        streamNoSymbols.push(strNoSymbols.trim());
-        streamNoSymbols.push(null);
-
+        
         const rl3 = readline.createInterface({
-            input: streamNoSymbols
+            input: getReadable(secondPass.getString().trim())
         });
-
+        
         //last pass to convert to binary
         rl3.on('line', (line) => {
             var commandType = Parser.commandType(line);
@@ -105,6 +71,8 @@ function getReadable(str) {
 
     return stream;
 }
+
+//TODO: Try to use some form of function composition for these, because they all have the same signature
 function runFirstPass() {
     var re = /(^\/\/.*)|(^\s*$)/gm;
     var lineNumber = 0; //Counter to keep track of what line number we are on, used so that labels specify correct line
@@ -138,6 +106,47 @@ function runFirstPass() {
 }
 
 function runSecondPass() {
+    var newline = '';
+    var symbolNumber = 16;  //First location to assign as address for variables
+    var strNoSymbols = '';    //For second pass to replace symbols
 
+    return {
+        parseLine: function(line) {
+            var commandType = Parser.commandType(line);    
+            if (commandType === 'A_COMMAND') {
+                var aSymbol = Parser.parseACommand(line);
+                //check if A-command is a symbol or number
+
+                if (isNaN(aSymbol)) {
+                    //We're looking at a symbol like @R0,
+                    //Check if symbol already exists, usually for predefined symbols
+                    if (SymbolTable.contains(aSymbol)) {
+                        newline = '@' + SymbolTable.getAddress(aSymbol);                
+                    } else { //change add to symbol table new variables
+                        newline = '@' + symbolNumber;
+                        SymbolTable.addEntry (aSymbol, symbolNumber++);
+                    }
+                } else {
+                    newline = line;
+                }
+            } else {
+                newline = line;
+            }
+            strNoSymbols += newline + '\n'; 
+        },
+
+        getString: function() {
+            return strNoSymbols;
+        }
+    }
+     
+}
+
+function runThirdPass() {
+    var strHack = '';       //For last pass to convert to binary
+
+    return {
+
+    }
 }
 
